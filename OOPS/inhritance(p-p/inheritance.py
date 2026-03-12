50169e62c75e3894895338074967c7e214f82604
Surya Gowda <suryagowda6415@gmail.com>
2026-03-12 09:21:31 +0530

    inheritance

---
+++ OOPS/inhritance(p-p/inheritance.py
@@ -0,0 +1,11 @@
+class Parent:
+    def __init__(self):
+        self.a=10
+class Child(Parent):
+    def __init__(self):
+        Parent.__init__(self)
+        self.b=20
+
+c1=Child()
+print(c1.a)
+print(c1.b)
